11516fb8a58d2be0d6ff166a42cfd0c78e0dcef8
Pesh <dev@zerelabs.com>
2026-07-05 02:54:29 +0530

    fix: add sw.js self-destruct script

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(() => {
+      self.registration.unregister();
+    })
+  );
+});
